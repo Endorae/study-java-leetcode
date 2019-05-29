7bc9ecf376a9a3260b9358f183c8350f9432e76f
meredith.f.lei@newegg.com <meredith.f.lei@newegg.com>
2019-05-29 13:38:58 +0800

    s.indexOf(ch, start) 版本

--- leetcode/src/main/java/lengthOfLongestSubstring/LengthOfLongestSubstring.java
+++ leetcode/src/main/java/lengthOfLongestSubstring/LengthOfLongestSubstring.java
@@ -1,119 +1,51 @@
 package lengthOfLongestSubstring;
 
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class LengthOfLongestSubstring {
 
-	 Map<Character,ArrayList<Integer>> charMap = null;
+	
 	 public int lengthOfLongestSubstring(String s) {
-		 		 
-		 if(s.length() <= 1)
+		 if(s == null || s.length() <= 0)
 		 {
-			 return s.length();
+			 return 0;
+		 }	
+		 
+		 int result = 1;	
+		 		 		 
+		 int length = 1;		 
+		 int start = 0;		 		 
+		
+		 
+		 for(int i= 0; i < s.length();){			 
+			 if(start + result > s.length())
+		     {
+		    	 break;
+		     }
+			 if(i>= s.length() - 1)
+			 {
+				 length = i - start  + 1;	
+				 if(length > result){
+						result = length;
+					}
+			 }
+			 char ch = s.charAt(i);	
+			 
+			 int indexOfChar = 	s.indexOf(ch, start);		
+			 
+			 if(indexOfChar != i) {				 
+				 length = i - start ;	
+			     start = indexOfChar + 1;
+			     i = start;
+			     
+			     if(length > result){
+						result = length;
+					}			     			     
+			 }
+			 else {
+				i++;						
+			 }				 
 		 }
 		 
-		 setCharMap(s);
-				
-		 Collection<ArrayList<Integer>> coll = charMap.values();
-		
-		 List<ArrayList<Integer>> indexlist = coll.stream().collect(Collectors.toList());
-				
-		 int maxLength = getLength(indexlist);
-		 			        
-		 return maxLength;
-		 
-	    }
-	 
-	 private int getLength(List<ArrayList<Integer>> list)
-	 {	
-		
-		int maxLength = 1;
-		
-		int start = -1;
-	    int lenght = 0;
-	    
-		for(int i= 0; i < list.size(); )
-		{	
-			ArrayList<Integer> charIndexList =  list.get(i);
-			
-			if(charIndexList != null && charIndexList.size() > 0)
-			{
-				Integer index = charIndexList.get(0);
-				
-				
-				if(index == start + 1)
-				{
-					lenght++;
-															
-					charIndexList.remove(0);
-					
-					if(charIndexList.size() == 0)
-					{
-						list.remove(i);
-					}
-					else
-					{
-						list.set(i, charIndexList);
-						i++;
-					}	
-					
-					start = index;
-				}
-				else
-				{ 
-					if(lenght > 0)
-					{
-						if(lenght > maxLength)
-						{
-							maxLength = lenght;
-						}
-						i = 0;
-						lenght = 0;
-						
-					}	
-					else
-					{
-						i++;
-					}
-				}
-																							
-				if(lenght >= list.size())
-				{
-					maxLength = lenght;
-					break;
-				}
-			}
-			
-			
-		}
-		return maxLength;
-		
-		
-	 }
-	 private void setCharMap(String s)
-	 {
-		
-		 char [] stringArr = s.toCharArray();
-		 charMap = new HashMap<Character,ArrayList<Integer>> (stringArr.length);
-		 
-		 for(int i=0; i < stringArr.length;i++ )
-			{	
-			    char ch = stringArr[i];
-			 	ArrayList<Integer> charIndexList = charMap.get(ch);
-				if(charIndexList == null)
-				{
-					charIndexList = new ArrayList<Integer> ();
-				  
-				}	
-				charIndexList.add(i);
-				charMap.put(ch, charIndexList);
-			}
+		 return length;
 	 }
 	
 }
--- leetcode/src/main/java/lengthOfLongestSubstring/LengthOfLongestSubstringTest.java
+++ leetcode/src/main/java/lengthOfLongestSubstring/LengthOfLongestSubstringTest.java
@@ -15,6 +15,7 @@ class LengthOfLongestSubstringTest {
 	@MethodSource("providerLengthOfLongestSubstringTestCase")	
 	void test(String input,int exceptedResult) {
 		
+		int test = "abcdefgh".indexOf('e', 2);
 		int result = new LengthOfLongestSubstring().lengthOfLongestSubstring(input);
 		assertEquals(exceptedResult,result); 
 	}
@@ -22,16 +23,16 @@ class LengthOfLongestSubstringTest {
 	@SuppressWarnings("unused")
 	private static Stream<Arguments> providerLengthOfLongestSubstringTestCase() {
 	    return Stream.of(
-	    		Arguments.of("ABCDE", 5)
-//	    		Arguments.of("a", 1),
-//	    		Arguments.of("aa", 1),
-//	    		Arguments.of("aA", 2),
-//	    		Arguments.of("AABB", 2),
-//	    		Arguments.of("", 0),
-//	    		Arguments.of("dvdf", 3),
-//	    		Arguments.of("ABCDCEEFADEAB", 4),
-//	    		Arguments.of("abcdefghijkmlnopqrstuvwxyz1234567890!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkmlnopqrstuvwxyz1234567890!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ", 72)
-//	    		
+	    		Arguments.of("ABCDE", 5),
+	    		Arguments.of("a", 1),
+	    		Arguments.of("aa", 1),
+	    		Arguments.of("aA", 2),
+	    		Arguments.of("AABB", 2),
+	    		Arguments.of("", 0),
+	    		Arguments.of("dvdf", 3),
+	    		Arguments.of("ABCDCEEFADEAB", 4),
+	    		Arguments.of("abcdefghijkmlnopqrstuvwxyz1234567890!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijkmlnopqrstuvwxyz1234567890!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ", 72)
+	    		
 				);	    		
 	}	
 }
