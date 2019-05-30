55a0a2fe99dcd434d91cca4caea762ebda6e8859
Endorae <47278456@qq.com>
2019-05-30 21:30:26 +0800

    merged

--- leetcode/src/main/java/lengthOfLongestSubstring/LengthOfLongestSubstring.java
+++ leetcode/src/main/java/lengthOfLongestSubstring/LengthOfLongestSubstring.java
@@ -1,119 +1,32 @@
 package lengthOfLongestSubstring;
 
-
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class LengthOfLongestSubstring {
 
-	 Map<Character,ArrayList<Integer>> charMap = null;
-	 public int lengthOfLongestSubstring(String s) {
-		 		 
-		 if(s.length() <= 1)
-		 {
-			 return s.length();
-		 }
-		 
-		 setCharMap(s);
-				
-		 Collection<ArrayList<Integer>> coll = charMap.values();
+	public int lengthOfLongestSubstring(String s) {
 		
-		 List<ArrayList<Integer>> indexlist = coll.stream().collect(Collectors.toList());
-				
-		 int maxLength = getLength(indexlist);
-		 			        
-		 return maxLength;
-		 
-	    }
-	 
-	 private int getLength(List<ArrayList<Integer>> list)
-	 {	
+		int fromIndex = 0;
+		int length = 0;
+		int maxlength = 0;
 		
-		int maxLength = 1;
+		int stringLength = s.length();
 		
-		int start = -1;
-	    int lenght = 0;
-	    
-		for(int i= 0; i < list.size(); )
-		{	
-			ArrayList<Integer> charIndexList =  list.get(i);
+		for(int i= 0; fromIndex < stringLength - maxlength; i++) {
 			
-			if(charIndexList != null && charIndexList.size() > 0)
-			{
-				Integer index = charIndexList.get(0);
-				
-				
-				if(index == start + 1)
-				{
-					lenght++;
-															
-					charIndexList.remove(0);
-					
-					if(charIndexList.size() == 0)
-					{
-						list.remove(i);
-					}
-					else
-					{
-						list.set(i, charIndexList);
-						i++;
-					}	
-					
-					start = index;
-				}
-				else
-				{ 
-					if(lenght > 0)
-					{
-						if(lenght > maxLength)
-						{
-							maxLength = lenght;
-						}
-						i = 0;
-						lenght = 0;
+			int pos = s.indexOf(s.charAt(i),fromIndex);
 						
-					}	
-					else
-					{
-						i++;
-					}
-				}
-																							
-				if(lenght >= list.size())
-				{
-					maxLength = lenght;
-					break;
-				}
+			if (pos != i) {				
+				
+				fromIndex = pos + 1;
 			}
 			
+ 			length = i - fromIndex + 1;
+			if(length > maxlength)
+			{
+				maxlength = length;
+			}
 			
 		}
-		return maxLength;
-		
-		
-	 }
-	 private void setCharMap(String s)
-	 {
-		
-		 char [] stringArr = s.toCharArray();
-		 charMap = new HashMap<Character,ArrayList<Integer>> (stringArr.length);
-		 
-		 for(int i=0; i < stringArr.length;i++ )
-			{	
-			    char ch = stringArr[i];
-			 	ArrayList<Integer> charIndexList = charMap.get(ch);
-				if(charIndexList == null)
-				{
-					charIndexList = new ArrayList<Integer> ();
-				  
-				}	
-				charIndexList.add(i);
-				charMap.put(ch, charIndexList);
-			}
-	 }
+		return maxlength;
+	}
 	
 }
